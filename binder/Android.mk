abbb5401c3b8fd305e9895814d144aa95eec9dc7
Jason <bdhien@humaxdigital.com>
2024-05-27 10:19:36 +0700

    Update binder

--- binder/Android.mk
+++ binder/Android.mk
@@ -1,5 +1,2 @@
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-
-include vendor/brcm/rpi4/binder/binder_c/Android.mk
-include vendor/brcm/rpi4/binder/binder_cpp/Android.mk
+include vendor/arpi/example/binder_c/Android.mk
+include vendor/arpi/example/binder_cpp/Android.mk
